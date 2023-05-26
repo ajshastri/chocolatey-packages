a8af9683aff00b74ea4909a0ec027f370f6eeeea
ajshastri <jayaramaditya1515@gmail.com>
2023-05-26 10:15:05 -0700

    Added zulu-8 8.0.372 chocolatey package

---
+++ zulu-8/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url64      = 'https://cdn.azul.com/zulu/bin/zulu8.70.0.23-ca-jdk8.0.372-win_x64.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'zulu-8*'
+  checksum64    = '1bb63988bf2bad3c81fbad613d80cfed2b91379ff8e114dc72048639a726f58d'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
