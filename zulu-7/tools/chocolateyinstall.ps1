fdc8f815b0a5cf2beb5322aa027800c545826f7c
ajshastri <jayaramaditya1515@gmail.com>
2023-06-07 08:38:48 -0700

    Added Zulu 7

---
+++ zulu-7/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url64      = 'https://cdn.azul.com/zulu/bin/zulu7.56.0.11-ca-jdk7.0.352-win_x64.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'Zulu 7*'
+  checksum64    = '029213c61b6de5439989bf561ba938cf6055ee7eb4ca52d03a46b20f83375606'
+  checksumType64= 'sha256'
+
+  silentArgs    = "ADDLOCAL=ZuluInstallation,FeatureEnvironment /qn /norestart"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
