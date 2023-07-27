4d9a7fd73f83161582e3ae05d350909f48f9b40c
ajshastri <jayaramaditya1515@gmail.com>
2023-07-27 15:58:29 -0700

    Updated zulu to the latest LTS.

---
+++ zulu/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url64      = 'https://cdn.azul.com/zulu/bin/zulu17.44.15-ca-fx-jdk17.0.8-win_x64.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'Zulu*'
+  checksum64    = '7df52260d890d7573f69f5a18dd8b751bcc5ef5da6803a122966c1da19c27707'
+  checksumType64= 'sha256'
+
+  silentArgs    = "ADDLOCAL=ZuluInstallation,FeatureEnvironment /qn /norestart"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
