e839522b37cd9ff027ea965cabeb662f817b1646
ajshastri <jayaramaditya1515@gmail.com>
2023-09-20 02:31:19 -0700

    Added zulu-21

---
+++ zulu-21/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url64      = 'https://cdn.azul.com/zulu/bin/zulu21.28.85-ca-fx-jdk21.0.0-win_x64.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'Zulu 21*'
+  checksum64    = '27d1b299c445a14483520bf8854c8a4081b6498af22bbe2acfa4dfb150128480'
+  checksumType64= 'sha256'
+
+  silentArgs    = "ADDLOCAL=ZuluInstallation,FeatureEnvironment /qn /norestart"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
