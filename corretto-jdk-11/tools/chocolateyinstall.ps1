3fdaddb7269758b6639bf2174c949b9c45b635f3
Aditya Shastri <linearreg224@gmail.com>
2024-01-17 11:48:44 -0800

    Attept to trigger corretto choco push

--- corretto-jdk-11/tools/chocolateyinstall.ps1
+++ corretto-jdk-11/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $packageArgs = @{
   PackageName = $env:ChocolateyPackageName
   Url64bit = 'https://corretto.aws/downloads/resources/11.0.22.7.1/amazon-corretto-11.0.22.7.1-windows-x64-jdk.msi'
-  Checksum64 = '59c529c07d2a79896ab67bd04d1e8fef'
+  Checksum64 = '59c529c07d2a79896ab627bd04d1e8fef'
   ChecksumType64 = 'md5'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"
