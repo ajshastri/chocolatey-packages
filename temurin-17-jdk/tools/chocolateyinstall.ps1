65ca7904084683af4ec5ba6abc402c312aaa46e6
ajshastri <jayaramaditya1515@gmail.com>
2024-01-27 13:05:14 -0800

    Revert "Temurin version update on Thu Jan 18 20:13:02 CUT 2024"
    This reverts commit cbfb600b2ed3a82a0c406260e6274306bd55ef8c.

--- temurin-17-jdk/tools/chocolateyinstall.ps1
+++ temurin-17-jdk/tools/chocolateyinstall.ps1
@@ -9,10 +9,10 @@ $packageArgs = @{
   PackageName     = 'Temurin17'
   fileType        = 'msi'
   Url             = 'https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.9%2B9.1/OpenJDK17U-jdk_x86-32_windows_hotspot_17.0.9_9.msi'
-  Url64bit        = 'https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.10%2B7/OpenJDK17U-jdk_x64_windows_hotspot_17.0.10_7.msi'
-  Checksum        = 'c23979ca982cc301a62143dd0ca75cefa75c33d029295a6dd21b75b01df2ba4e'
+  Url64bit        = 'https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.9%2B9.1/OpenJDK17U-jdk_x64_windows_hotspot_17.0.9_9.msi'
+  Checksum        = 'C23979CA982CC301A62143DD0CA75CEFA75C33D029295A6DD21B75B01DF2BA4E'
   ChecksumType    = 'sha256'
-  Checksum64      = 'd45b610b1800e35fd667d144277272e3323c7fafa21c0312649fc60a86913a3d'
+  Checksum64      = 'CD7B319F6FBD7EFC68A0E464C55C7F2A28D6B8BE3D0BCDA315A16F885C57CADB'
   ChecksumType64  = 'sha256'
   SilentArgs      = $pp
 }
