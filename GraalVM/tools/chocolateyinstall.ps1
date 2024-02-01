dacac0e3b6fe8f1318e18d0518102f04d293d34a
ajshastri <jayaramaditya1515@gmail.com>
2024-01-31 23:31:59 -0800

    Graalvm update to 21

---
+++ GraalVM/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$programFiles = (${env:ProgramFiles}, ${env:ProgramFiles(x86)} -ne $null)[0]
+$installDir = "$programFiles\GraalVM"
+$version = "21.0.2+13.1"
+
+$packageArgs = @{
+    PackageName = $env:ChocolateyPackageName
+    UnzipLocation = $targetDir = $installDir
+    Url64bit = 'https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-21.0.2/graalvm-community-jdk-21.0.2_windows-x64_bin.zip'
+    Checksum64 = 'e17b7bead097bf372a5c75df17815b0a2f30b777a019d25eff7706b21421f7fa'
+    ChecksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\graalvm-ce-java17-$version 'Machine'
+# The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
+Install-ChocolateyPath $targetDir\graalvm-ce-java17-$version\bin -PathType 'Machine'
