623cf2ec0a1d9aa7a0a278bda6abda82b864b704
ajshastri <jayaramaditya1515@gmail.com>
2024-05-29 15:29:18 -0700

    Upgraded zulu to java 21

--- zulu/tools/chocolateyinstall.ps1
+++ zulu/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url64      = 'https://cdn.azul.com/zulu/bin/zulu17.48.15-ca-fx-jdk17.0.10-win_x64.msi'
+$url64      = 'https://cdn.azul.com/zulu/bin/zulu21.34.19-ca-fx-jdk21.0.3-win_x64.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,7 +11,7 @@ $packageArgs = @{
   url64bit      = $url64
 
   softwareName  = 'Zulu*'
-  checksum64    = 'b6b97ccace9c6e878394d173f757f1c093e41d9e08b02fd807f35e204643a7d7'
+  checksum64    = 'aa938976ce2a689a4479f0b0747092dd7736addc419b3489c7d66ff87761ef34'
   checksumType64= 'sha256'
 
   silentArgs    = "ADDLOCAL=ZuluInstallation,FeatureEnvironment /qn /norestart"
