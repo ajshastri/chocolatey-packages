d5de62eba5560d5c515c43e8970a37a4354bd847
ajshastri <jayaramaditya1515@gmail.com>
2024-06-01 10:28:46 -0700

    Fixed corretto 21

--- corretto-jdk-21/tools/chocolateyinstall.ps1
+++ corretto-jdk-21/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://corretto.aws/downloads/resources/21.0.2.14.1/amazon-corretto-21.0.2.14.1-windows-x64.msi'
+  Url64bit = 'https://corretto.aws/downloads/resources/21.0.3.9.1/amazon-corretto-21.0.3.9.1-windows-x64.msi'
   Checksum64 = 'b6c850fb368107f4f662b46cb0f1fdea'
   ChecksumType64 = 'md5'
   fileType      = 'msi'
--- corretto-jdk/tools/chocolateyinstall.ps1
+++ corretto-jdk/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://corretto.aws/downloads/resources/21.0.2.14.1/amazon-corretto-21.0.2.14.1-windows-x64.msi'
-  Checksum64 = '58899be77c0992cce93172389de1cbeb8'
+  Url64bit = 'https://corretto.aws/downloads/resources/21.0.3.9.1/amazon-corretto-21.0.3.9.1-windows-x64.msi'
+  Checksum64 = 'b6c850fb368107f4f662b46cb0f1fdea'
   ChecksumType64 = 'md5'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"
