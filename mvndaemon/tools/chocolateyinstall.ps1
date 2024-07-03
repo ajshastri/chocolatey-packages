06f106987f697e77fa6af78a4e8b7b3bbf22a542
ajshastri <jayaramaditya1515@gmail.com>
2024-07-03 09:37:06 -0700

    Updated mvnd to 1.0.1

--- mvndaemon/tools/chocolateyinstall.ps1
+++ mvndaemon/tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $installDir = "$programFiles\mvndaemon"
 $packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
     UnzipLocation    = $targetDir = $installDir
-    Url64            = 'https://github.com/apache/maven-mvnd/releases/download/1.0.0/maven-mvnd-1.0.0-windows-amd64.zip'
-    Checksum64       = '0ccc62bd1633f9655cef6ff11a081424a639e1a2d023535322967e1419c4638b'
+    Url64            = 'https://github.com/apache/maven-mvnd/releases/download/1.0.1/maven-mvnd-1.0.1-windows-amd64.zip'
+    Checksum64       = '5a4a05fa6394b6fca0f88e18aa87682e64ce41923b057ca611ea25eb4405d34c'
     ChecksumType64   = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyPath $targetDir\maven-mvnd-1.0.0-windows-amd64\bin -PathType 'Machine'
+Install-ChocolateyPath $targetDir\maven-mvnd-1.0.1-windows-amd64\bin -PathType 'Machine'
