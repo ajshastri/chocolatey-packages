4fd592234ff8d1c158adb307e30db69d27620936
ajshastri <jayaramaditya1515@gmail.com>
2024-07-24 21:16:11 -0700

    Updated Graalvm

--- GraalVM/tools/chocolateyinstall.ps1
+++ GraalVM/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 $programFiles = (${env:ProgramFiles}, ${env:ProgramFiles(x86)} -ne $null)[0]
 $installDir = "$programFiles\GraalVM"
-$version = "22.0.1+8.1"
+$version = "22.0.2+9.1"
 
 $packageArgs = @{
     PackageName = $env:ChocolateyPackageName
     UnzipLocation = $targetDir = $installDir
-    Url64bit = 'https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-22.0.1/graalvm-community-jdk-22.0.1_windows-x64_bin.zip'
-    Checksum64 = '623a4c5984f1210e61346e3ff942ec6f83d1928790ef9ae7dd28067e5c8de1aa'
+    Url64bit = 'https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-22.0.2/graalvm-community-jdk-22.0.2_windows-x64_bin.zip'
+    Checksum64 = '107a37cea666c2ad6ad9eaa408b4041f822a24071a33c5057762ce48833e99a3'
     ChecksumType64 = 'sha256'
 }
 
