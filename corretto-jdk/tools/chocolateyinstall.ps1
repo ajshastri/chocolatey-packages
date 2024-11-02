ea10ccb5de5338dbcbdef7be231c45853b2f0082
ajshastri <jayaramaditya1515@gmail.com>
2024-11-01 22:28:03 -0700

    Correto fix

--- corretto-jdk/tools/chocolateyinstall.ps1
+++ corretto-jdk/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 $packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://corretto.aws/downloads/resources/21.0.4.7.1/amazon-corretto-21.0.4.7.1-windows-x64.msi'
+  Url64bit = 'https://corretto.aws/downloads/resources/21.0.5.11.1/amazon-corretto-21.0.5.11.1-windows-x64.msi'
   Checksum64 = 'e7359e0a738e150cdc5feb0487c1e31c'
   ChecksumType64 = 'md5'
   fileType      = 'msi'
