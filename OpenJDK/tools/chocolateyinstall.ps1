507253c974f78991fcf649a5bd281b7f3842dd99
ajshastri <jayaramaditya1515@gmail.com>
2025-09-27 21:30:08 -0700

    OpenJDK Update

--- OpenJDK/tools/chocolateyinstall.ps1
+++ OpenJDK/tools/chocolateyinstall.ps1
@@ -2,13 +2,13 @@ $ErrorActionPreference = 'Stop'
 $programFiles = (${env:ProgramFiles}, ${env:ProgramFiles(x86)} -ne $null)[0]
 $installDir = "$programFiles\OpenJDK"
 
-$version = "22.0.2"
+$version = "25.0.0"
 
 $packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
     UnzipLocation    = $targetDir = $installDir
-    Url64            = 'https://download.java.net/java/GA/jdk22.0.2/c9ecb94cd31b495da20a27d4581645e8/9/GPL/openjdk-22.0.2_windows-x64_bin.zip'
-    Checksum64       = 'f2a9b9ab944e71a64637fcdc6b13a1188cf02d4eb9ecf71dc927e98b3e45f5dc'
+    Url64            = 'https://download.java.net/java/GA/jdk25/bd75d5f9689641da8e1daabeccb5528b/36/GPL/openjdk-25_windows-x64_bin.zip'
+    Checksum64       = '85bcc178461e2cb3c549ab9ca9dfa73afd54c09a175d6510d0884071867137d3'
     ChecksumType64   = 'sha256'
 }
 
--- OpenJDK/tools/chocolateyuninstall.ps1
+++ OpenJDK/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 $programFiles = (${env:ProgramFiles}, ${env:ProgramFiles(x86)} -ne $null)[0]
 $installDir = "$programFiles\OpenJDK"
 
-$version = '22.0.2'
+$version = '25.0.0'
 
 Uninstall-ChocolateyEnvironmentVariable 'JAVA_HOME' 'Machine'
 rm -r "$installDir\jdk-$version"
