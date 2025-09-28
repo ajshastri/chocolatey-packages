b7c36c7a06e0a658c2717d3fceefb3665d70b82b
Aditya Shastri <linearreg224@gmail.com>
2025-09-27 22:19:28 -0700

    Update temurin 21

--- temurin-21-jdk/tools/chocolateyinstall.ps1
+++ temurin-21-jdk/tools/chocolateyinstall.ps1
@@ -7,8 +7,8 @@ $parameters = (Get-PackageParameters); $pp = ( Test-PackageParamaters $parameter
 
 $packageArgs = @{
   PackageName    = 'Temurin21'
-  Url64bit       = ''
-  Checksum64     = ''
+  Url64bit       = 'https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.8%2B9/OpenJDK21U-jdk_x64_windows_hotspot_21.0.8_9.msi'
+  Checksum64     = 'd82030e8689b19efedfbce50ce38351ca81b302c06936584c6a27bda18339df8'
   ChecksumType64 = 'sha256'
   fileType       = 'msi'
   SilentArgs     = $pp
--- temurin-21-jre/tools/chocolateyinstall.ps1
+++ temurin-21-jre/tools/chocolateyinstall.ps1
@@ -7,8 +7,8 @@ $parameters = (Get-PackageParameters); $pp = ( Test-PackageParamaters $parameter
 
 $packageArgs = @{
   PackageName    = 'Temurin21jre'
-  Url64bit       = ''
-  Checksum64     = ''
+  Url64bit       = 'https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.8%2B9/OpenJDK21U-jre_x64_windows_hotspot_21.0.8_9.msi'
+  Checksum64     = '9e38a94eed115fb834c2c4375bc493cef1a47e28c82bd83f623efe3fca017e7a'
   ChecksumType64 = 'sha256'
   fileType       = 'msi'
   SilentArgs     = $pp
