371dec45a54bda1345c7ddf24b43ab1863b6d566
Aditya Shastri <linearreg224@gmail.com>
2025-09-27 22:51:05 -0700

    Update Wildfly

--- WildFly/tools/chocolateyinstall.ps1
+++ WildFly/tools/chocolateyinstall.ps1
@@ -5,9 +5,9 @@ $installDir = "$programFiles\WildFly"
 $packageArgs = @{
     PackageName = $env:ChocolateyPackageName
     UnzipLocation = $targetDir = $installDir
-    Url = 'https://github.com/wildfly/wildfly/releases/download/33.0.0.Final/wildfly-33.0.0.Final.zip'
-    Checksum = '6fe811183dd1cb6e369f93c5ec3b4c4ac3c99266'
-    ChecksumType = 'sha1'
+    Url = 'https://github.com/wildfly/wildfly/releases/download/37.0.1.Final/wildfly-37.0.1.Final.zip'
+    Checksum = 'bbb9722f7d55e07f034f02ec08f9902c5262eb6ce2bb61bc929cba3cf08c054c'
+    ChecksumType = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
