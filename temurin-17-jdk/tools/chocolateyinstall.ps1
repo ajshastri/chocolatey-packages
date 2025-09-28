73a509d462cc01c9a0f558abf6c36622ea9ff858
Aditya Shastri <linearreg224@gmail.com>
2025-09-27 22:16:33 -0700

    Update Temurin 17

--- temurin-17-jdk/tools/chocolateyinstall.ps1
+++ temurin-17-jdk/tools/chocolateyinstall.ps1
@@ -8,11 +8,11 @@ $parameters = (Get-PackageParameters); $pp = ( Test-PackageParamaters $parameter
 $packageArgs = @{
   PackageName     = 'Temurin17'
   fileType        = 'msi'
-  Url             = ''
-  Url64bit        = ''
-  Checksum        = ''
+  Url             = 'https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.16%2B8/OpenJDK17U-jdk_x86-32_windows_hotspot_17.0.16_8.msi'
+  Url64bit        = 'https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.16%2B8/OpenJDK17U-jdk_x64_windows_hotspot_17.0.16_8.msi'
+  Checksum        = '3e3516535d75af5fa14471300980ca86be209ed7fbf31a3c24c6f5c5c96ffc50'
   ChecksumType    = 'sha256'
-  Checksum64      = ''
+  Checksum64      = '1c3701f074d75036650731bcf6b08d69b1e3567bfa853f0ddd7aaba44da5b595'
   ChecksumType64  = 'sha256'
   SilentArgs      = $pp
 }
--- temurin-17-jre/tools/chocolateyinstall.ps1
+++ temurin-17-jre/tools/chocolateyinstall.ps1
@@ -8,11 +8,11 @@ $parameters = (Get-PackageParameters); $pp = ( Test-PackageParamaters $parameter
 $packageArgs = @{
   PackageName     = 'Temurin17jre'
   fileType        = 'msi'
-  Url             = ''
-  Url64bit        = ''
-  Checksum        = ''
+  Url             = 'https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.16%2B8/OpenJDK17U-jre_x86-32_windows_hotspot_17.0.16_8.msi'
+  Url64bit        = 'https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.16%2B8/OpenJDK17U-jre_x64_windows_hotspot_17.0.16_8.msi'
+  Checksum        = '47ac8df108d911103a053880a401fc18302b4b9a098814436716deba86cdfb76'
   ChecksumType    = 'sha256'
-  Checksum64      = ''
+  Checksum64      = '9937d754d7157dcdb7ec70a83a5e6238ce093c71316435b4dd07ae38880980d2'
   ChecksumType64  = 'sha256'
   SilentArgs      = $pp
 }
