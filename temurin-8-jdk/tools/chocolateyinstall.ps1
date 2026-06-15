ebeb430d9a9356b7d6cb84cb637b971325f9fae8
Christoph Kisfeld <spam@b-web.org>
2026-06-15 17:04:34 +0200

    Revert "Temurin version update on Tue Dec  9 00:56:55 UTC 2025"
    This reverts commit 3ddb24f690a3f7323422b71e5def763afc334c8d.

--- temurin-8-jdk/tools/chocolateyinstall.ps1
+++ temurin-8-jdk/tools/chocolateyinstall.ps1
@@ -8,11 +8,11 @@ $parameters = (Get-PackageParameters); $pp = ( Test-PackageParamaters $parameter
 $packageArgs = @{
   PackageName     = 'Temurin8'
   fileType        = 'msi'
-  Url             = ''
-  Url64bit        = ''
-  Checksum        = ''
+  Url             = 'https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u462-b08/OpenJDK8U-jdk_x86-32_windows_hotspot_8u462b08.msi'
+  Url64bit        = 'https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u472-b08/OpenJDK8U-jdk_x64_windows_hotspot_8u472b08.msi'
+  Checksum        = 'bde76537146dddddafa6bcaeb603af173e4144084f8ab3337d575da84604a72b'
   ChecksumType    = 'sha256'
-  Checksum64      = ''
+  Checksum64      = '810c04469e75c2f1cf83091e9dc78497b84e48ad21269291d9b7ff59b5cbb404'
   ChecksumType64  = 'sha256'
   SilentArgs      = $pp
 }
